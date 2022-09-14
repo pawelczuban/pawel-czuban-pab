d4953b2af99b57fd65ec806c1bbd207dd358b581
pawelczuban <80459075+pawelczuban@users.noreply.github.com>
2022-09-14 23:10:09 +0200

    index update

--- restauracja/index.ts
+++ restauracja/index.ts
@@ -1,6 +1,13 @@
-const express = require('express')  
-const app = express()  
-app.get('/', function (req, res) {  
-  res.send('Hello World')  
-})  
+import  express  from "express";
+import mongoose from "mongoose";
+import { main } from "./db";
+
+const app = express();
+app.use(express.json())
+
+main();
+
+const danie = require('./routes/danieRouter');
+app.use('/dania', danie);
+
 app.listen(3000)
